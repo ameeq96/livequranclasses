7ad6618e061bc2176633584f842af6742fca3dc3
ameeqkhan <ameeqkhan183@gmail.com>
2026-02-18 00:37:17 +0500

    remove

--- routes/web.php
+++ routes/web.php
@@ -14,27 +14,6 @@ Route::permanentRedirect('/blog', '/blogs');
 Route::permanentRedirect('/blog/detail', '/blogs/how-to-start-quran-lessons-online-free-and-build-consistency');
 Route::permanentRedirect('/contact.html', '/contact');
 
-Route::get('/sitemap.xml', function () {
-    $urls = [
-        ['loc' => route('home'), 'priority' => '1.0', 'changefreq' => 'weekly'],
-        ['loc' => route('about'), 'priority' => '0.8', 'changefreq' => 'monthly'],
-        ['loc' => route('faq'), 'priority' => '0.8', 'changefreq' => 'monthly'],
-        ['loc' => route('services'), 'priority' => '0.8', 'changefreq' => 'monthly'],
-        ['loc' => route('courses'), 'priority' => '0.9', 'changefreq' => 'weekly'],
-        ['loc' => route('enroll.show'), 'priority' => '0.9', 'changefreq' => 'weekly'],
-        ['loc' => route('blogs'), 'priority' => '0.8', 'changefreq' => 'weekly'],
-        ['loc' => route('blogs.detail', ['slug' => 'how-to-start-quran-lessons-online-free-and-build-consistency']), 'priority' => '0.7', 'changefreq' => 'monthly'],
-        ['loc' => route('contact'), 'priority' => '0.7', 'changefreq' => 'monthly'],
-    ];
-
-    $xml = view('sitemap.index', [
-        'urls' => $urls,
-        'lastmod' => now()->toAtomString(),
-    ])->render();
-
-    return response($xml, 200)->header('Content-Type', 'application/xml');
-})->name('sitemap');
-
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/about', [HomeController::class, 'about'])->name('about');
 Route::get('/faq', [HomeController::class, 'faq'])->name('faq');
