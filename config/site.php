baa6ee397c44fa12414ff5f1f7d0a40deb590660
ameeqkhan <ameeqkhan183@gmail.com>
2026-02-19 19:14:00 +0500

    change

--- config/site.php
+++ config/site.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'contact_email' => env('SITE_CONTACT_EMAIL', 'admission@livequranclasses.com'),
+    'contact_email' => env('SITE_CONTACT_EMAIL', 'admin@livequranclasses.com'),
     'contact_phone' => env('SITE_CONTACT_PHONE', '+1 (639) 390-3194'),
     'contact_address' => env('SITE_CONTACT_ADDRESS', 'SASKATOON SK, Canada'),
     'latitude' => env('SITE_LATITUDE', '37.7749'),
