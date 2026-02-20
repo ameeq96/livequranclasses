328add4f89934140af956bba00c0ab6c902405fe
ameeqkhan <ameeqkhan183@gmail.com>
2026-02-20 18:19:50 +0500

    add whatsapp

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,10 @@
 <html>
 @include('layouts.partials.head')
 <body>
+@php
+    $whatsAppNumber = preg_replace('/[^0-9]/', '', config('site.contact_phone', ''));
+    $whatsAppLink = $whatsAppNumber ? 'https://wa.me/' . $whatsAppNumber : '#';
+@endphp
 <div class="page-wrapper">
 
     <!-- Preloader -->
@@ -16,6 +20,10 @@
 
 </div>
 <!-- End PageWrapper -->
+
+<a href="{{ $whatsAppLink }}" class="whatsapp-float" target="_blank" rel="noopener" aria-label="Chat on WhatsApp">
+    <i class="fa-brands fa-whatsapp"></i>
+</a>
 
 @include('layouts.partials.scripts')
 @stack('scripts')
